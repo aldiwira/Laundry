61875899ec22cd71bc1a95f542225f99d085d609
aldiwira <aldiwira.aw@gmail.com>
2020-03-06 01:40:37 +0700

    add get and post users

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require("express");
 const apps = express();
 const bodyParse = require("body-parser");
 const dotenv = require("dotenv/config");
+const con = require("./config/db");
 const usersRoute = require("./routes/usersRouter");
+
+apps.use(bodyParse.json());
 
 apps.get("/", (req, res) => res.send("Welcome Laundry"));
 apps.use("/users", usersRoute);
+con
+  .authenticate()
+  .then(() => {
+    console.log("Connection established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  })
+  .finally();
 apps.listen(process.env.PORT_RUN, () => {
   console.log("http://" + process.env.DB_HOST + ":" + process.env.PORT_RUN);
 });
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,36 @@
 const express = require("express");
+
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("Heloo");
+
+const usersModel = require("../model/usersModel");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await usersModel.findAll();
+    res.json({
+      status: 200,
+      massage: "Success fetch data.",
+      datas: users
+    });
+  } catch (error) {
+    res.json({ massage: error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const users = await usersModel
+    .create({
+      nama: req.body.nama,
+      no_headphone: req.body.no_headphone,
+      alamat: req.body.alamat,
+      role: req.body.role
+    })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json({ massage: err });
+    });
 });
 
 module.exports = router;
