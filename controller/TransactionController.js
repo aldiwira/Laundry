1ebde62f16968f997f4dc10d7bdb983c98bbb3cc
aldiwira <aldiwira.aw@gmail.com>
2020-03-12 16:07:10 +0700

    add json format at order

--- controller/TransactionController.js
+++ controller/TransactionController.js
@@ -3,6 +3,7 @@ const priceModel = require("../models/PriceModel");
 const userModel = require("../models/UsersModel");
 const detailTransaction = require("../models/DetailTransactionModels");
 const { queryTypes } = require("sequelize");
+let uniqid = require("uniqid");
 
 transactionModel.hasMany(detailTransaction, { foreignKey: "no_nota" });
 
@@ -22,8 +23,35 @@ module.exports = {
         include: transactionDetail
       })
       .then(datas => {
-        res.status(200).json(datas);
+        res.status(200).json({
+          status: 200,
+          message: "Success Load Datas",
+          data: datas
+        });
+      })
+      .catch(err => {
+        res.status(401).json({
+          status: 401,
+          message: "Failed Get Order",
+          data: err
+        });
       });
   },
-  processCreateTransaction: async (req, res) => {}
+  processCreateTransaction: async (req, res) => {
+    let id_detail_transaction = uniqid.time();
+    let transaction_detil = {
+      id_detail_transaction,
+      bobot: 0,
+      status: false,
+      no_nota: uniqid.time(),
+      id_harga: req.body.id_harga
+    };
+    let transaction_node = {
+      id_detail_transaction,
+      total_tagihan: 0,
+      pembayaran: 0,
+      status_pembayaran: 0,
+      id_user: req.body.id_user
+    };
+  }
 };
--- routes/TransactionRouter.js
+++ routes/TransactionRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const transactionModel = require("../controller/TransactionController");
+const transactionControl = require("../controller/TransactionController");
 
-router.get("/", transactionModel.processFetchTransaction);
+router.get("/", transactionControl.processFetchTransaction);
+router.post("/", transactionControl.processCreateTransaction);
 
 module.exports = router;
