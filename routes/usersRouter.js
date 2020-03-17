479abc29b65d5eb26e63c0184396c555fabc143a
aldiwira <aldiwira.aw@gmail.com>
2020-03-17 12:32:44 +0700

    fix user controller

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controller/UsersController.js");
+const userController = require("../controller/usersController.js");
 
 router.post("/register", userController.processRegisterPhone);
 router.post("/register/account", userController.processRegisterAccount);
