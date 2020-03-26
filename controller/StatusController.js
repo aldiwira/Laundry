4db14f3ea1a0bac6f0805c9df67d836fac899a99
aldiwira <aldiwira.aw@gmail.com>
2020-03-26 11:45:40 +0700

    add route for get status or transaction by user id

--- controller/StatusController.js
+++ controller/StatusController.js
@@ -4,8 +4,7 @@ const userModel = require("../models/UsersModel.js");
 const detailTransaction = require("../models/DetailTransactionModels.js");
 const { queryTypes, Op } = require("sequelize");
 const response = require("./response");
-let uniqid = require("uniqid");
-let bcrypt = require("bcrypt");
+const url = require("url");
 let code;
 let data;
 let message;
@@ -15,7 +14,6 @@ detailTransaction.belongsTo(transactionModel);
 module.exports = {
   processFetchDataStatus: async (req, res) => {
     let no_nota = req.body.no_nota;
-    const price = [{ model: priceModel, attributes: ["kelas"] }];
     const transactionDetail = [
       {
         model: detailTransaction,
@@ -30,6 +28,37 @@ module.exports = {
         where: {
           no_nota: {
             [Op.like]: "%" + no_nota
+          }
+        }
+      })
+      .then(datas => {
+        code = response.CODE_SUCCESS;
+        data = datas;
+        message = "Success Load Datas";
+      })
+      .catch(err => {
+        code = response.CODE_FAILURE;
+        data = err;
+        message = "Failed Load Datas";
+      });
+    res.status(code).send(response.set(code, message, data));
+  },
+  processFetchDataByIdUser: async (req, res) => {
+    let id_user = req.params.id_user;
+    const transactionDetail = [
+      {
+        model: detailTransaction,
+        attributes: ["id_detail_transaction", "status", "bobot", "id_harga"]
+      }
+    ];
+    const field = ["no_nota", "status_pembayaran", "createdAt", "updatedAt"];
+    await transactionModel
+      .findAll({
+        attributes: field,
+        include: transactionDetail,
+        where: {
+          id_user: {
+            [Op.like]: "%" + id_user
           }
         }
       })
